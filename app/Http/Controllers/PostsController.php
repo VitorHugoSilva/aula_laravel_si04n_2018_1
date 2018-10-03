46113b3465aa68305602645aab8a162c5e4775d0
Vitor Hugo <vhsilva.ap@gmail.com>
2018-10-03 20:47:09 -0300

    Aula sobre Model e Eloquent

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Post;
+
 class PostsController extends Controller
 {
 
    	public function index()
     {
-    	return view('posts.index');
+    	$posts = Post::all();
+    	return view('posts.index', compact('posts'));
     }
 
     public function show($id)
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,7 +7,20 @@ Pagina de posts
 @section('content')
     <div class="row">
       <div class="col-md-12">
-      	<h1>Estou em posts</h1>
+      	<table class="table table-bordered table-hover table-striped">
+      		<tr>
+      			<th>ID</th>
+      			<th>Title</th>
+      			<th>Content</th>
+      		</tr>
+      		@foreach($posts as $post)
+      		<tr>
+      			<td>{{ $post->id }}</td>
+      			<td>{{ $post->title }}</td>
+      			<td>{{ $post->content }}</td>
+      		</tr>
+      		@endforeach
+      	</table>
       </div>
     </div>
 @endsection
